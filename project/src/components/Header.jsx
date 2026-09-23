41bf571906f6cd87a9fadedf8536e42549859744
YHN <nhuyce200087@gmail.com>
2026-09-23 08:16:13 +0700

    add header

---
+++ project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+    return (<header> <h1>My React Project</h1> <nav> <a href="/">Home</a> <a href="/about">About</a> <a href="/contact">Contact</a> </nav> </header>
+    );
+};
+
+export default Header;
